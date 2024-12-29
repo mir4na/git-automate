05d7c698eb679cf3e726a56ffe4fe2bfa313b23a
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2024-12-29 16:29:16 +0700

    pale-hill

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::process::{Command, exit};
-// use names::Generator;
+use names::Generator;
 
-fn pull() {
-    let pull = Command::new("git")
-    .arg("pull").arg("origin").arg("master").output()
-    .expect("failed to pull repo");
+// fn pull() {
+//     let pull = Command::new("git")
+//     .arg("pull").arg("origin").arg("master").output()
+//     .expect("failed to pull repo");
 
-    if !pull.status.success() {
-        eprintln!("Error: Failed to pull repo");
-        exit(1);
-    }
-}
+//     if !pull.status.success() {
+//         eprintln!("Error: Failed to pull repo");
+//         exit(1);
+//     }
+// }
 
 fn commit_push() {
     let add = Command::new("git")
@@ -22,9 +22,9 @@ fn commit_push() {
         exit(1);
     }
 
-    //let commit_message = generator();
+    let commit_message = generator();
     let commit = Command::new("git")
-        .arg("commit").arg("-m").arg("commit files").output()
+        .arg("commit").arg("-m").arg(commit_message).output()
         .expect("failed to git commit");
 
     if !commit.status.success() {
@@ -38,17 +38,17 @@ fn commit_push() {
 
     if !push.status.success() {
         eprintln!("Error: Failed to push files: {:?}", String::from_utf8_lossy(&push.stderr));
-        pull();
+        //pull();
         exit(1);
     }
 
     println!("Successfully pushed files to git repo!");
 }
 
-// fn generator() -> String {
-//     let mut generator = Generator::default();
-//     generator.next().unwrap()
-// }
+fn generator() -> String {
+    let mut generator = Generator::default();
+    generator.next().unwrap()
+}
 
 fn main() {
     commit_push();
